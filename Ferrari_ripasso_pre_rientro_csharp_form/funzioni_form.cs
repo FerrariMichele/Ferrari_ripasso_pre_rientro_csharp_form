46d9be640beb161d760e1b2739acb9f4f4de77f9
Michele Ferrari <Michele@0m3x7.onmicrosoft.com>
2023-08-28 14:36:42 +0200

    Aggiungere i file di progetto.

---
+++ Ferrari_ripasso_pre_rientro_csharp_form/funzioni_form.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferrari_ripasso_pre_rientro_csharp_form
+{
+    internal class funzioni_form
+    {
+    }
+}
