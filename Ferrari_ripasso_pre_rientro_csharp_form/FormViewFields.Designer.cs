d9ff466e14b9abbd0f25d3212191d1bbab1d2036
Michele Ferrari <Michele@0m3x7.onmicrosoft.com>
2023-09-07 16:09:32 +0200

    funzione di visualizzazione

--- Ferrari_ripasso_pre_rientro_csharp_form/FormViewFields.Designer.cs
+++ Ferrari_ripasso_pre_rientro_csharp_form/FormViewFields.Designer.cs
@@ -28,9 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormViewFields));
             this.panelListView = new System.Windows.Forms.Panel();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.panelCheckBoxes = new System.Windows.Forms.Panel();
+            this.labelCampi = new System.Windows.Forms.Label();
+            this.checkBoxMiovalore = new System.Windows.Forms.CheckBox();
+            this.checkBoxLatitudine = new System.Windows.Forms.CheckBox();
+            this.checkBoxLongitudine = new System.Windows.Forms.CheckBox();
+            this.checkBoxIdOSM = new System.Windows.Forms.CheckBox();
+            this.checkBoxData = new System.Windows.Forms.CheckBox();
+            this.checkBoxAnno = new System.Windows.Forms.CheckBox();
+            this.checkBoxNome = new System.Windows.Forms.CheckBox();
+            this.checkBoxRegione = new System.Windows.Forms.CheckBox();
+            this.checkBoxProvincia = new System.Windows.Forms.CheckBox();
+            this.checkBoxComune = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panelListView.SuspendLayout();
+            this.panelCheckBoxes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panelListView
@@ -39,19 +55,181 @@
             this.panelListView.Controls.Add(this.listView1);
             this.panelListView.Location = new System.Drawing.Point(12, 12);
             this.panelListView.Name = "panelListView";
-            this.panelListView.Size = new System.Drawing.Size(760, 537);
+            this.panelListView.Size = new System.Drawing.Size(537, 537);
             this.panelListView.TabIndex = 1;
             // 
             // listView1
             // 
-            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(239)))), ((int)(((byte)(239)))));
+            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(227)))), ((int)(((byte)(106)))));
             this.listView1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.GridLines = true;
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(12, 12);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(736, 513);
+            this.listView1.Size = new System.Drawing.Size(513, 513);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // panelCheckBoxes
+            // 
+            this.panelCheckBoxes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(203)))), ((int)(((byte)(255)))), ((int)(((byte)(140)))));
+            this.panelCheckBoxes.Controls.Add(this.labelCampi);
+            this.panelCheckBoxes.Controls.Add(this.checkBoxMiovalore);
+            this.panelCheckBoxes.Controls.Add(this.checkBoxLatitudine);
+            this.panelCheckBoxes.Controls.Add(this.checkBoxLongitudine);
+            this.panelCheckBoxes.Controls.Add(this.checkBoxIdOSM);
+            this.panelCheckBoxes.Controls.Add(this.checkBoxData);
+            this.panelCheckBoxes.Controls.Add(this.checkBoxAnno);
+            this.panelCheckBoxes.Controls.Add(this.checkBoxNome);
+            this.panelCheckBoxes.Controls.Add(this.checkBoxRegione);
+            this.panelCheckBoxes.Controls.Add(this.checkBoxProvincia);
+            this.panelCheckBoxes.Controls.Add(this.checkBoxComune);
+            this.panelCheckBoxes.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panelCheckBoxes.Location = new System.Drawing.Point(566, 12);
+            this.panelCheckBoxes.Name = "panelCheckBoxes";
+            this.panelCheckBoxes.Size = new System.Drawing.Size(200, 273);
+            this.panelCheckBoxes.TabIndex = 2;
+            // 
+            // labelCampi
+            // 
+            this.labelCampi.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCampi.Location = new System.Drawing.Point(3, 12);
+            this.labelCampi.Name = "labelCampi";
+            this.labelCampi.Size = new System.Drawing.Size(194, 26);
+            this.labelCampi.TabIndex = 10;
+            this.labelCampi.Text = "Campi selezionabili";
+            this.labelCampi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBoxMiovalore
+            // 
+            this.checkBoxMiovalore.AutoSize = true;
+            this.checkBoxMiovalore.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxMiovalore.Location = new System.Drawing.Point(6, 248);
+            this.checkBoxMiovalore.Name = "checkBoxMiovalore";
+            this.checkBoxMiovalore.Size = new System.Drawing.Size(77, 17);
+            this.checkBoxMiovalore.TabIndex = 9;
+            this.checkBoxMiovalore.Text = "Miovalore";
+            this.checkBoxMiovalore.UseVisualStyleBackColor = true;
+            this.checkBoxMiovalore.Click += new System.EventHandler(this.checkBoxMiovalore_Click);
+            // 
+            // checkBoxLatitudine
+            // 
+            this.checkBoxLatitudine.AutoSize = true;
+            this.checkBoxLatitudine.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxLatitudine.Location = new System.Drawing.Point(6, 225);
+            this.checkBoxLatitudine.Name = "checkBoxLatitudine";
+            this.checkBoxLatitudine.Size = new System.Drawing.Size(78, 17);
+            this.checkBoxLatitudine.TabIndex = 8;
+            this.checkBoxLatitudine.Text = "Latitudine";
+            this.checkBoxLatitudine.UseVisualStyleBackColor = true;
+            this.checkBoxLatitudine.Click += new System.EventHandler(this.checkBoxLatitudine_Click);
+            // 
+            // checkBoxLongitudine
+            // 
+            this.checkBoxLongitudine.AutoSize = true;
+            this.checkBoxLongitudine.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxLongitudine.Location = new System.Drawing.Point(6, 202);
+            this.checkBoxLongitudine.Name = "checkBoxLongitudine";
+            this.checkBoxLongitudine.Size = new System.Drawing.Size(89, 17);
+            this.checkBoxLongitudine.TabIndex = 7;
+            this.checkBoxLongitudine.Text = "Longitudine";
+            this.checkBoxLongitudine.UseVisualStyleBackColor = true;
+            this.checkBoxLongitudine.Click += new System.EventHandler(this.checkBoxLongitudine_Click);
+            // 
+            // checkBoxIdOSM
+            // 
+            this.checkBoxIdOSM.AutoSize = true;
+            this.checkBoxIdOSM.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxIdOSM.Location = new System.Drawing.Point(6, 179);
+            this.checkBoxIdOSM.Name = "checkBoxIdOSM";
+            this.checkBoxIdOSM.Size = new System.Drawing.Size(193, 17);
+            this.checkBoxIdOSM.TabIndex = 6;
+            this.checkBoxIdOSM.Text = "Identificatore in OpenStreetMap";
+            this.checkBoxIdOSM.UseVisualStyleBackColor = true;
+            this.checkBoxIdOSM.Click += new System.EventHandler(this.checkBoxIdOSM_Click);
+            // 
+            // checkBoxData
+            // 
+            this.checkBoxData.AutoSize = true;
+            this.checkBoxData.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxData.Location = new System.Drawing.Point(6, 156);
+            this.checkBoxData.Name = "checkBoxData";
+            this.checkBoxData.Size = new System.Drawing.Size(153, 17);
+            this.checkBoxData.TabIndex = 5;
+            this.checkBoxData.Text = "Data e Ora di Inerimento";
+            this.checkBoxData.UseVisualStyleBackColor = true;
+            this.checkBoxData.Click += new System.EventHandler(this.checkBoxData_Click);
+            // 
+            // checkBoxAnno
+            // 
+            this.checkBoxAnno.AutoSize = true;
+            this.checkBoxAnno.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxAnno.Location = new System.Drawing.Point(6, 133);
+            this.checkBoxAnno.Name = "checkBoxAnno";
+            this.checkBoxAnno.Size = new System.Drawing.Size(131, 17);
+            this.checkBoxAnno.TabIndex = 4;
+            this.checkBoxAnno.Text = "Anno di Inserimento";
+            this.checkBoxAnno.UseVisualStyleBackColor = true;
+            this.checkBoxAnno.Click += new System.EventHandler(this.checkBoxAnno_Click);
+            // 
+            // checkBoxNome
+            // 
+            this.checkBoxNome.AutoSize = true;
+            this.checkBoxNome.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxNome.Location = new System.Drawing.Point(6, 110);
+            this.checkBoxNome.Name = "checkBoxNome";
+            this.checkBoxNome.Size = new System.Drawing.Size(56, 17);
+            this.checkBoxNome.TabIndex = 3;
+            this.checkBoxNome.Text = "Nome";
+            this.checkBoxNome.UseVisualStyleBackColor = true;
+            this.checkBoxNome.Click += new System.EventHandler(this.checkBoxNome_Click);
+            // 
+            // checkBoxRegione
+            // 
+            this.checkBoxRegione.AutoSize = true;
+            this.checkBoxRegione.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxRegione.Location = new System.Drawing.Point(6, 87);
+            this.checkBoxRegione.Name = "checkBoxRegione";
+            this.checkBoxRegione.Size = new System.Drawing.Size(69, 17);
+            this.checkBoxRegione.TabIndex = 2;
+            this.checkBoxRegione.Text = "Regione";
+            this.checkBoxRegione.UseVisualStyleBackColor = true;
+            this.checkBoxRegione.Click += new System.EventHandler(this.checkBoxRegione_Click);
+            // 
+            // checkBoxProvincia
+            // 
+            this.checkBoxProvincia.AutoSize = true;
+            this.checkBoxProvincia.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxProvincia.Location = new System.Drawing.Point(6, 64);
+            this.checkBoxProvincia.Name = "checkBoxProvincia";
+            this.checkBoxProvincia.Size = new System.Drawing.Size(72, 17);
+            this.checkBoxProvincia.TabIndex = 1;
+            this.checkBoxProvincia.Text = "Provincia";
+            this.checkBoxProvincia.UseVisualStyleBackColor = true;
+            this.checkBoxProvincia.Click += new System.EventHandler(this.checkBoxProvincia_Click);
+            // 
+            // checkBoxComune
+            // 
+            this.checkBoxComune.AutoSize = true;
+            this.checkBoxComune.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxComune.Location = new System.Drawing.Point(6, 41);
+            this.checkBoxComune.Name = "checkBoxComune";
+            this.checkBoxComune.Size = new System.Drawing.Size(69, 17);
+            this.checkBoxComune.TabIndex = 0;
+            this.checkBoxComune.Text = "Comune";
+            this.checkBoxComune.UseVisualStyleBackColor = true;
+            this.checkBoxComune.Click += new System.EventHandler(this.checkBoxComune_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(566, 291);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 258);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // FormViewFields
             // 
@@ -59,10 +237,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(239)))), ((int)(((byte)(239)))));
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panelCheckBoxes);
             this.Controls.Add(this.panelListView);
             this.Name = "FormViewFields";
             this.Text = "FormViewFields";
+            this.Load += new System.EventHandler(this.FormViewFields_Load);
             this.panelListView.ResumeLayout(false);
+            this.panelCheckBoxes.ResumeLayout(false);
+            this.panelCheckBoxes.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -70,5 +254,18 @@
         #endregion
         private System.Windows.Forms.Panel panelListView;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Panel panelCheckBoxes;
+        private System.Windows.Forms.CheckBox checkBoxIdOSM;
+        private System.Windows.Forms.CheckBox checkBoxData;
+        private System.Windows.Forms.CheckBox checkBoxAnno;
+        private System.Windows.Forms.CheckBox checkBoxNome;
+        private System.Windows.Forms.CheckBox checkBoxRegione;
+        private System.Windows.Forms.CheckBox checkBoxProvincia;
+        private System.Windows.Forms.CheckBox checkBoxComune;
+        private System.Windows.Forms.CheckBox checkBoxMiovalore;
+        private System.Windows.Forms.CheckBox checkBoxLatitudine;
+        private System.Windows.Forms.CheckBox checkBoxLongitudine;
+        private System.Windows.Forms.Label labelCampi;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Ferrari_ripasso_pre_rientro_csharp_form/FormViewFields.cs
+++ Ferrari_ripasso_pre_rientro_csharp_form/FormViewFields.cs
@@ -15,31 +15,66 @@ namespace Ferrari_ripasso_pre_rientro_csharp_form
     {
         #region dichiarazioni
         funzioni_form functions;
-        string fileName, fileNameTemp;
-        private Color backColor;
-        private Color foreColor;
+        string fileName;
         #endregion
         public FormViewFields()
         {
             InitializeComponent();
             functions = new funzioni_form();
             fileName = "ferrari.csv";
-            viewData();
         }
-        private void viewData()
+        private void FormViewFields_Load(object sender, EventArgs e)
+        {
+            if (functions.countFields(fileName) == 9)
+                checkBoxMiovalore.Visible = false;
+            else
+                checkBoxMiovalore.Visible = true;
+        }
+        private int checkCheckBoxNumber()
+        {
+            int checkedNumber = 0;
+            if (checkBoxComune.Checked) checkedNumber++;
+            if (checkBoxProvincia.Checked) checkedNumber++;
+            if (checkBoxRegione.Checked) checkedNumber++;
+            if (checkBoxNome.Checked) checkedNumber++;
+            if (checkBoxAnno.Checked) checkedNumber++;
+            if (checkBoxData.Checked) checkedNumber++;
+            if (checkBoxIdOSM.Checked) checkedNumber++;
+            if (checkBoxLatitudine.Checked) checkedNumber++;
+            if (checkBoxLongitudine.Checked) checkedNumber++;
+            if (checkBoxMiovalore.Checked) checkedNumber++;
+            return checkedNumber;
+        }
+        private void viewItems(string fileName)
         {
             listView1.Clear();
             int numFields = functions.countFields(fileName);
-            string[] columns = functions.searchFieldsNames(fileName);
+            int[] checkEd = new int[numFields];
+            for (int i = 0; i < numFields; i++)
+                checkEd[i] = 0;
+            if (checkBoxComune.Checked) checkEd[0] = 1;
+            if (checkBoxProvincia.Checked) checkEd[1] = 1;
+            if (checkBoxRegione.Checked) checkEd[2] = 1;
+            if (checkBoxNome.Checked) checkEd[3] = 1;
+            if (checkBoxAnno.Checked) checkEd[4] = 1;
+            if (checkBoxData.Checked) checkEd[5] = 1;
+            if (checkBoxIdOSM.Checked) checkEd[6] = 1;
+            if (checkBoxLongitudine.Checked) checkEd[7] = 1;
+            if (checkBoxLatitudine.Checked) checkEd[8] = 1;
+            if (numFields > 9)
+                if (checkBoxMiovalore.Checked) checkEd[9] = 1;
+            string[] names = functions.searchFieldsNames(fileName);
+            listView1.View = View.Details;
+            listView1.GridLines = true;
             using (StreamReader csvReader = File.OpenText(fileName))
             {
                 string lineFromFile;
-                listView1.View = View.Details;
-                for (int i = 0; i < columns.Length; i++)
+                
+                for (int i = 0; i < numFields; i++)
                 {
-                    listView1.Columns.Add(columns[i], listView1.Width / 3, HorizontalAlignment.Center);
+                    if (checkEd[i] == 1)
+                        listView1.Columns.Add(names[i], listView1.Width / checkCheckBoxNumber(), HorizontalAlignment.Center);
                 }
-                listView1.GridLines = true;
                 lineFromFile = csvReader.ReadLine();
                 while ((lineFromFile = csvReader.ReadLine()) != null)
                 {
@@ -49,10 +84,16 @@ namespace Ferrari_ripasso_pre_rientro_csharp_form
                         if (fields[10] == "0")
                         {
                             ListViewItem newItem = new ListViewItem();
-                            newItem.Text = fields[0];
-                            for (int j = 1; j < columns.Length - 1; j++)
+                            bool first = false;
+                            for (int i = 0; i < numFields; i++)
                             {
-                                newItem.SubItems.Add(fields[j]);
+                                if (checkEd[i] == 1 && !first)
+                                {
+                                    newItem.Text = fields[i];
+                                    first = true;
+                                }
+                                else if (checkEd[i] == 1)
+                                    newItem.SubItems.Add(fields[i]);
                             }
                             listView1.Items.Add(newItem);
                         }
@@ -60,15 +101,191 @@ namespace Ferrari_ripasso_pre_rientro_csharp_form
                     else
                     {
                         ListViewItem newItem = new ListViewItem();
-                        newItem.Text = fields[0];
-                        for (int j = 1; j < columns.Length; j++)
+                        bool first = false;
+                        for (int i = 0; i < numFields; i++)
                         {
-                            newItem.SubItems.Add(fields[j]);
+                            if (checkEd[i] == 1 && !first)
+                            {
+                                newItem.Text = fields[i];
+                                first = true;
+                            }
+                            else if (checkEd[i] == 1)
+                                newItem.SubItems.Add(fields[i]);
                         }
                         listView1.Items.Add(newItem);
                     }
                 }
                 csvReader.Close();
+            }
+        }
+        private void checkBoxComune_Click(object sender, EventArgs e)
+        {
+            int checkBoxNumber = checkCheckBoxNumber();
+            if (checkBoxNumber > 3)
+            {
+                MessageBox.Show("Selezionare massimo 3 campi", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                checkBoxComune.Checked = false;
+            }
+            else if (checkBoxNumber > 0)
+            {
+                viewItems(fileName);
+            }
+            else
+            {
+                listView1.Clear();
+            }
+        }
+        private void checkBoxProvincia_Click(object sender, EventArgs e)
+        {
+            int checkBoxNumber = checkCheckBoxNumber();
+            if (checkBoxNumber > 3)
+            {
+                MessageBox.Show("Selezionare massimo 3 campi", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                checkBoxProvincia.Checked = false;
+            }
+            else if (checkBoxNumber > 0)
+            {
+                viewItems(fileName);
+            }
+            else
+            {
+                listView1.Clear();
+            }
+        }
+        private void checkBoxRegione_Click(object sender, EventArgs e)
+        {
+            int checkBoxNumber = checkCheckBoxNumber();
+            if (checkBoxNumber > 3)
+            {
+                MessageBox.Show("Selezionare massimo 3 campi", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                checkBoxRegione.Checked = false;
+            }
+            else if (checkBoxNumber > 0)
+            {
+                viewItems(fileName);
+            }
+            else
+            {
+                listView1.Clear();
+            }
+        }
+        private void checkBoxNome_Click(object sender, EventArgs e)
+        {
+            int checkBoxNumber = checkCheckBoxNumber();
+            if (checkBoxNumber > 3)
+            {
+                MessageBox.Show("Selezionare massimo 3 campi", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                checkBoxNome.Checked = false;
+            }
+            else if (checkBoxNumber > 0)
+            {
+                viewItems(fileName);
+            }
+            else
+            {
+                listView1.Clear();
+            }
+        }
+        private void checkBoxAnno_Click(object sender, EventArgs e)
+        {
+            int checkBoxNumber = checkCheckBoxNumber();
+            if (checkBoxNumber > 3)
+            {
+                MessageBox.Show("Selezionare massimo 3 campi", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                checkBoxAnno.Checked = false;
+            }
+            else if (checkBoxNumber > 0)
+            {
+                viewItems(fileName);
+            }
+            else
+            {
+                listView1.Clear();
+            }
+        }
+        private void checkBoxData_Click(object sender, EventArgs e)
+        {
+            int checkBoxNumber = checkCheckBoxNumber();
+            if (checkBoxNumber > 3)
+            {
+                MessageBox.Show("Selezionare massimo 3 campi", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                checkBoxData.Checked = false;
+            }
+            else if (checkBoxNumber > 0)
+            {
+                viewItems(fileName);
+            }
+            else
+            {
+                listView1.Clear();
+            }
+        }
+        private void checkBoxIdOSM_Click(object sender, EventArgs e)
+        {
+            int checkBoxNumber = checkCheckBoxNumber();
+            if (checkBoxNumber > 3)
+            {
+                MessageBox.Show("Selezionare massimo 3 campi", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                checkBoxIdOSM.Checked = false;
+            }
+            else if (checkBoxNumber > 0)
+            {
+                viewItems(fileName);
+            }
+            else
+            {
+                listView1.Clear();
+            }
+        }
+        private void checkBoxLongitudine_Click(object sender, EventArgs e)
+        {
+            int checkBoxNumber = checkCheckBoxNumber();
+            if (checkBoxNumber > 3)
+            {
+                MessageBox.Show("Selezionare massimo 3 campi", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                checkBoxLongitudine.Checked = false;
+            }
+            else if (checkBoxNumber > 0)
+            {
+                viewItems(fileName);
+            }
+            else
+            {
+                listView1.Clear();
+            }
+        }
+        private void checkBoxLatitudine_Click(object sender, EventArgs e)
+        {
+            int checkBoxNumber = checkCheckBoxNumber();
+            if (checkBoxNumber > 3)
+            {
+                MessageBox.Show("Selezionare massimo 3 campi", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                checkBoxLatitudine.Checked = false;
+            }
+            else if (checkBoxNumber > 0)
+            {
+                viewItems(fileName);
+            }
+            else
+            {
+                listView1.Clear();
+            }
+        }
+        private void checkBoxMiovalore_Click(object sender, EventArgs e)
+        {
+            int checkBoxNumber = checkCheckBoxNumber();
+            if (checkBoxNumber > 3)
+            {
+                MessageBox.Show("Selezionare massimo 3 campi", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                checkBoxMiovalore.Checked = false;
+            }
+            else if (checkBoxNumber > 0)
+            {
+                viewItems(fileName);
+            }
+            else
+            {
+                listView1.Clear();
             }
         }
     }
